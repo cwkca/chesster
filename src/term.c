a2c4878617155d9fb9aed8c51c98521bc20c442d
cwkca <43796917+cwkca@users.noreply.github.com>
2026-05-28 09:19:10 -0700

    Adding terminal UI (#1)

---
+++ src/term.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+const char *pieces[] = {
+    " ",
+    "♗", "♔", "♘", "♙", "♕", "♖",
+    "♝", "♚", "♞", "♟", "♛", "♜"};
+
+const int board[8][8] = {
+    12, 9, 7, 11, 8, 7, 9, 12,
+    10, 10, 10, 10, 10, 10, 10, 10,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    4, 4, 4, 4, 4, 4, 4, 4,
+    6, 3, 1, 5, 2, 1, 3, 6};
+
+#define BUFFER_LEN 20
+char piece_str_1[BUFFER_LEN], piece_str_2[BUFFER_LEN];
+
+void format_piece(char *buffer, int rank, int file, char invert)
+{
+    int piece_index = board[rank][file];
+    if (invert)
+    {
+        if (piece_index)
+        {
+            if (piece_index > 6)
+                piece_index -= 6;
+            else
+                piece_index += 6;
+        }
+
+        snprintf(buffer, BUFFER_LEN, "\033[7m%s\033[0m", pieces[piece_index]);
+    }
+    else
+    {
+        strncpy(buffer, pieces[piece_index], BUFFER_LEN);
+    }
+}
+
+void draw_chessboard()
+{
+    printf("┌───▄▄▄───▄▄▄───▄▄▄───▄▄▄▖\n");
+
+    int rank, file;
+    for (rank = 0; rank < 8; rank += 2)
+    {
+        printf("│");
+        for (file = 0; file < 8; file += 2)
+        {
+            format_piece(piece_str_1, rank, file, 0);
+            format_piece(piece_str_2, rank, file + 1, 1);
+            printf(" %s █%s█", piece_str_1, piece_str_2);
+        }
+        printf("▌\n▗▄▄▄▀▀▀▄▄▄▀▀▀▄▄▄▀▀▀▄▄▄▀▀▀▘\n▐");
+        for (file = 0; file < 8; file += 2)
+        {
+            format_piece(piece_str_1, rank + 1, file, 1);
+            format_piece(piece_str_2, rank + 1, file + 1, 0);
+            printf("█%s█ %s ", piece_str_1, piece_str_2);
+        }
+        printf("│\n");
+
+        if (rank < 6)
+            printf("▝▀▀▀▄▄▄▀▀▀▄▄▄▀▀▀▄▄▄▀▀▀▄▄▄▖\n");
+    }
+    printf("▝▀▀▀───▀▀▀───▀▀▀───▀▀▀───┘\n");
+}
+
+int main(int argc, char **argv)
+{
+    draw_chessboard();
+    return 0;
+}
